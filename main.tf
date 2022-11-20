ab26e4ed9e9e0971df090ce783ea31bc5b2ddd2e
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2022-11-20 18:47:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY A VM IN AWS
-#-----------------------------------------------------------------------------------------------------------------------
-#
-
 provider "aws" {
   access_key = "AKIA4P56N3H6C5HDZ676"
   secret_key = "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"
   region     = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    encrypt = false
+    bucket= "tf-statefile-demo"
+    dynamodb_table = "tf-state-lock-dynamo"
+    key = "path/path/terraform-tfstate"
+    region = "us-east-1"
+    
+  }
+} 
 
 resource "aws_instance" "example" {
   ami           = "ami-00b6a8a2bd28daf19"
